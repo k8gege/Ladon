eb328f4928ce68fa7edb234963abe29846c3da4b
k8gege <42312878+k8gege@users.noreply.github.com>
2019-12-10 19:04:27 +0800

    Update AssemblyInfo.cs

--- Ladon/Properties/AssemblyInfo.cs
+++ Ladon/Properties/AssemblyInfo.cs
@@ -5,6 +5,7 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
+
 [assembly: AssemblyTitle("")]
 [assembly: AssemblyDescription("Ladon by k8gege")]
 [assembly: AssemblyConfiguration("")]
